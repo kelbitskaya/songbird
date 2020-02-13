1e1695f7dd9827a7f082c60c21b5c86adc4bce16
Maria_Veremey <Maria_Veremey@epam.com>
2020-02-13 16:16:27 +0300

    remove app from master

--- src/components/main/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = (props) => {
-  const {
-    buttonclass, value, disabled, handleClick,
-  } = props;
-  return (<button type="button" className={`page-block page__block_with-border button ${buttonclass}`} disabled={disabled} onClick={handleClick}>{value}</button>);
-};
-export default Button;
